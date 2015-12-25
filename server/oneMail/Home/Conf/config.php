ee8d3aac69f1b91d6fbbb61eacb1cc35dc4e5278
liaohui1080 <liaohui1080@tianliaohui.com>
2015-12-25 16:20:49 +0800

    第一次正式提交 ,修改了 url模式=3 ,修改了 js连接服务器的参数 /?s=/

--- server/oneMail/Home/Conf/config.php
+++ server/oneMail/Home/Conf/config.php
@@ -2,19 +2,19 @@
 return array(
     /* 数据库设置 */
     'DB_TYPE'                => 'mysql', // 数据库类型
-    'DB_HOST'                => '127.0.0.1', // 服务器地址
+    'DB_HOST'                => 'localhost', // 服务器地址
     'DB_NAME'                => 'one_mail', // 数据库名
     'DB_USER'                => 'root', // 用户名
     'DB_PWD'                 => '13525232487', // 密码
     'DB_PORT'                => '3306', // 端口
     'DB_PREFIX'              => 'mail_', // 数据库表前缀
-    'DB_DEBUG'               => true, // 数据库调试模式 开启后可以记录SQL日志
-    'DB_FIELDS_CACHE'        => false, // 启用字段缓存
+    'DB_DEBUG'               => false, // 数据库调试模式 开启后可以记录SQL日志
+    'DB_FIELDS_CACHE'        => true, // 启用字段缓存
     'DB_CHARSET'             => 'utf8', // 数据库编码默认采用utf8
 
     /* URL设置 */
     'URL_CASE_INSENSITIVE'   => false, // 默true 表示URL不区分大小写 false则表示区分大小写
-    'URL_MODEL'              => 2, // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
+    'URL_MODEL'              => 3, // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
     // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式
 
     'MULTI_MODULE'           => false, // 是否允许多模块 如果为false 则必须设置 DEFAULT_MODULE
